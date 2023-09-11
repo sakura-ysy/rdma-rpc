72456396622795a0c5bb40af32b098d9dba0b2b6
sakura-ysy <2575633193@qq.com>
2023-09-11 04:18:11 -0700

    fix the disconnect bug

---
+++ build/src/CMakeFiles/rdma-lib.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rdma-lib.dir/client.cc.o"
+  "CMakeFiles/rdma-lib.dir/client.cc.o.d"
+  "CMakeFiles/rdma-lib.dir/connection.cc.o"
+  "CMakeFiles/rdma-lib.dir/connection.cc.o.d"
+  "CMakeFiles/rdma-lib.dir/context.cc.o"
+  "CMakeFiles/rdma-lib.dir/context.cc.o.d"
+  "CMakeFiles/rdma-lib.dir/handler.cc.o"
+  "CMakeFiles/rdma-lib.dir/handler.cc.o.d"
+  "CMakeFiles/rdma-lib.dir/message.cc.o"
+  "CMakeFiles/rdma-lib.dir/message.cc.o.d"
+  "CMakeFiles/rdma-lib.dir/server.cc.o"
+  "CMakeFiles/rdma-lib.dir/server.cc.o.d"
+  "librdma-lib.a"
+  "librdma-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rdma-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
